334dbbe7318273648f64b079bd9b5660396bc63e
Sam Trettin <trettinftw@gmail.com>
2018-12-16 18:58:38 -0800

    configuring settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-BASE_DIR = 'locallibrary'
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'vpe+123k4i#cb=+4b@4(g#rc-rzv=59zz-kd4k)_!1r=9otg7k'
 import os
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -149,3 +149,5 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+print('basedir is at ' + BASE_DIR)
